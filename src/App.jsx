d27afbec366f7e034c8097dfe8bac927d3bdff29
Joan D <joandidachr@gmail.comm>
2024-09-05 00:49:06 +0200

    Initial commit with setup, tailwind config, tailwind classes and initial navbar, NOT stable

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Header from './assets/components/Header';
+
+const App = () => {
+  return (
+    <>
+     
+        <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+          <Header/>
+        </div>
+      <ButtonGradient/>
+    </>
+  );
+}
+
+export default App;
