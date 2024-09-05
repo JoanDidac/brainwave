dcc232300bcfe75d28461ae739252ecfffba2869
Joan D <joandidachr@gmail.comm>
2024-09-05 12:22:36 +0200

    improves the navbar functionality + Svg icon conditional rendering/formatting working! , Stable version, not merged to master

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,46 @@
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
-import Button from './Button'
+import Button from './Button';
+import  MenuSvg  from '../../src/assets/svg/MenuSvg';
+import { HamburgerMenu } from "./design/Header";
 
 const Header = () => {
   const pathname = useLocation(); //Review the URl you're currently on
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+    } else {
+      setOpenNavigation(true);
+    }
+  }
+
+  const handleClick = () => {
+    setOpenNavigation(false);
+  }
 
   return (
     <div
-      className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b
-     border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm"
+      className={`fixed top-0 left-0 w-full z-50 border-b 
+     border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+        }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8 " href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
 
-        <nav className="hidden top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:flex lg:mx-auto lg:bg-transparent">
+        <nav className={` ${openNavigation ? 'flex':'hidden'} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:flex lg:mx-auto lg:bg-transparent`}>
           <div className="relative z-2 flex flex-col items-center justify-cenmter m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
                 key={item.id}
                 href={item.url}
+                onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors
                 hover:text-color-1 ${
                 item.onlyMobile ? "lg:hidden" : ""
@@ -33,7 +51,9 @@ const Header = () => {
                 {item.title}
               </a>
             ))}
+
           </div>
+            <HamburgerMenu/>
         </nav>
         <a href="#signup" className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
@@ -43,7 +63,12 @@ const Header = () => {
         <Button className="hidden lg:flex" href="#login">
           Sign In
 
-              </Button>
+        </Button>
+        <Button className="ml-auto lg:hidden" px="px-3" onClick={toggleNavigation}>
+
+           <MenuSvg openNavigation={openNavigation} />
+        </Button>
+
 
       </div>
     </div>
