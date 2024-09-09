1b03f702bc5d3040d3957f3529a43c1c08d30ebc
Joan D <joandidachr@gmail.comm>
2024-09-09 12:32:13 +0200

    Improves layout of container + image + second robot image, Stable Version

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,56 +1,74 @@
-
 import { service1, service2, service3, check } from "../assets";
-import { brainwaveServices , brainwaveServicesIcons } from "../constants";
-import Heading from './Heading'
-import Section from './Section'
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
+import Generating from "./Generating";
+import Heading from "./Heading";
+import Section from "./Section";
 
 const Services = () => {
-  return<div>
+  return (
+    <div>
       <Section id="how-to-use">
-          <div className='container'>
-              <Heading title="Generative AI made for Creators." text="Brainwave unlocks te potential of AI-powered applications." />
-              <div className='relative'>
-                  <div className='relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]'>
-                      <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 lg:w-auto">
-                          <img 
-                              className="w-full h-full object-cover md:object-right "
-                              src={service1}
-                               width={800}
-                              height={730}
-                              alt='Smartest AI'
-                             
-                          /> 
-                      </div>
-
-                      <div className="relative z-1 max-w-[17rem] ml-auto">
-                          <h4 className="h-4 mb-4">Smartest AI </h4>
-                          <p className="body-2 mb-[3rem] text-n-3 ">Brainwave unlocks the potential of AI powered applications.</p>
-                          <ul className="body-2">
-                              {brainwaveServices.map((item, index) => (
-                                  <li
-                                      className="flex items-start py-4 border-t border-n-6"
-                                      key={index}>
-                                      <img 
-                                          width={24}
-                                          height={24}
-                                          src={check}
-                                      />
-                                      <p className="ml-4">{item}</p>
-                                      
-                                  </li>
-                              ))}
-                              
-                          </ul>
-                          
-                      </div>
-                      
-                  </div>
-                  
+        <div className="container">
+          <Heading
+            title="Generative AI made for Creators."
+            text="Brainwave unlocks te potential of AI-powered applications."
+          />
+          <div className="relative">
+            <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+              <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 lg:w-auto">
+                <img
+                  className="w-full h-full object-cover md:object-right "
+                  src={service1}
+                  width={800}
+                  height={730}
+                  alt="Smartest AI"
+                />
               </div>
-          </div>    
+
+              <div className="relative z-1 max-w-[17rem] ml-auto">
+                <h4 className="h-4 mb-4">Smartest AI </h4>
+                <p className="body-2 mb-[3rem] text-n-3 ">
+                  Brainwave unlocks the potential of AI powered applications.
+                </p>
+                <ul className="body-2">
+                  {brainwaveServices.map((item, index) => (
+                    <li
+                      className="flex items-start py-4 border-t border-n-6"
+                      key={index}
+                    >
+                      <img width={24} height={24} src={check} />
+                      <p className="ml-4">{item}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
+            </div>
+            <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+              <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+                <div className="absolute inset-0">
+                  <img
+                    className="h-full w-full object-cover"
+                    width={630}
+                    height={750}
+                    alt="robotito"
+                    src={service2}
+                  />
+                </div>
+
+                <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                  <h4 className="h-4 mb-4">Photo Editing</h4>
+                  <p className="body-2 mb-[3rem] text-n-3 ">
+                    Automatically enhance your photos using our AI app's photo editing feature. Try it now! 
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Section>
     </div>
-  
-}
+  );
+};
 
-export default Services
+export default Services;
